410c0252e9576d05e8578d917cfe5e26cb3d9985
Xurshid <109508707+sotiboldiyevxurshid@users.noreply.github.com>
2023-01-06 17:33:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@
 
 
 // function App() {
+const [count, setCount] = useState(0); // useState returns a pair. 'count' is the current state. 'setCount' is a function we can use to update the state.
+
+  function increment() {
+    //setCount(prevCount => prevCount+=1);
+    setCount(function (prevCount) {
+      return (prevCount += 1);
+    });
+  }
+
+  function decrement() {
+    setCount(function (prevCount) {
+      if (prevCount > 0) {
+        return (prevCount -= 1); 
+      } else {
+        return (prevCount = 0);
+      }
+    });
+  }
 
 //   let name = "Bekhruz"
 //   let surname = "Ergashev"
@@ -12,6 +30,9 @@
 //     <>
     
 //       <Header name={name} surname={surname} number={number} email={email}/>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
  
 //     </>
